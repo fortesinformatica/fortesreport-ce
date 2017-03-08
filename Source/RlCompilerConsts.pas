86cf3269edefb416a225fa0b8e07865cf2cf40fb
vejrous <jiri.vejda@gmail.com>
2017-03-08 20:38:00 +0100

    Replaced Tab characters for Space characters

--- Source/RlCompilerConsts.pas
+++ Source/RlCompilerConsts.pas
@@ -52,29 +52,29 @@ unit RlCompilerConsts;
 interface
 
 const
-  cvDelphi1       =	8;    //VER80
+  cvDelphi1       = 8;    //VER80
   cvDelphi2       = 9;    //VER90
-  cvDelphi3       = 10;	  //VER100
-  cvDelphi4       = 12;	  //VER120
-  cvDelphi5       = 13;	  //VER130
-  cvDelphi6       = 14;	  //VER140
-  cvDelphi7       = 15;	  //VER150
-  cvDelphi8NET	  = 16;	  //VER160
-  cvDelphi2005    = 17;	  //VER170
-  cvDelphi2006    = 18;	  //VER180
-  cvDelphi2007    = 18.5;	//VER185
+  cvDelphi3       = 10;   //VER100
+  cvDelphi4       = 12;   //VER120
+  cvDelphi5       = 13;   //VER130
+  cvDelphi6       = 14;   //VER140
+  cvDelphi7       = 15;   //VER150
+  cvDelphi8NET    = 16;   //VER160
+  cvDelphi2005    = 17;   //VER170
+  cvDelphi2006    = 18;   //VER180
+  cvDelphi2007    = 18.5; //VER185
   cvDelphi2007NET = 19;   //VER190
-  cvDelphi2009    = 20;	  //VER200
-  cvDelphi2010    = 21;	  //VER210
-  cvDelphiXE      = 22;	  //VER220
-  cvDelphiXE2	    = 23;   //VER230
-  cvDelphiXE3	    = 24;   //VER240
-  cvDelphiXE4	  = 25;   //VER250
-  cvDelphiXE5	  = 26;   //VER260
-  cvDelphiXE6	  = 27;   //VER270
-  cvDelphiXE7	  = 28;   //VER280
-  cvDelphiXE8	  = 29;   //VER290
-  cvDelphiXE9	  = 25;   //VER300 Seatle
+  cvDelphi2009    = 20;   //VER200
+  cvDelphi2010    = 21;   //VER210
+  cvDelphiXE      = 22;   //VER220
+  cvDelphiXE2     = 23;   //VER230
+  cvDelphiXE3     = 24;   //VER240
+  cvDelphiXE4     = 25;   //VER250
+  cvDelphiXE5     = 26;   //VER260
+  cvDelphiXE6     = 27;   //VER270
+  cvDelphiXE7     = 28;   //VER280
+  cvDelphiXE8     = 29;   //VER290
+  cvDelphiXE9     = 25;   //VER300 Seatle
 
 implementation
 end.
