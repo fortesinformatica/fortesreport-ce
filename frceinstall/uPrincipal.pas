e29f6dc725b81ace4015fab4564cc02df438b627
juliomar <juliomar_m@yahoo.com.br>
2020-05-27 01:37:18 -0300

    Delphi 10.4 Sydney
    Compatibilização dos fontes e instalador.

--- frceinstall/uPrincipal.pas
+++ frceinstall/uPrincipal.pas
@@ -532,7 +532,7 @@ begin
      if VersionNumberStr = 'd16' then
         Sender.Options.Add('-NSData.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell;System;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win');
 
-     if MatchText(VersionNumberStr, ['d17','d18','d19','d20','d21','d22','d23','d24','d25','d26']) then
+     if MatchText(VersionNumberStr, ['d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27']) then
         Sender.Options.Add('-NSWinapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;System;Xml;Data;Datasnap;Web;Soap;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell');
 
   end;
@@ -595,7 +595,9 @@ begin
     else if oFRCE.Installations[iFor].VersionNumberStr = 'd25' then
       edtDelphiVersion.Items.Add('Delphi 10.2 Tokyo')
     else if oFRCE.Installations[iFor].VersionNumberStr = 'd26' then
-      edtDelphiVersion.Items.Add('Delphi 10.3 Rio');
+      edtDelphiVersion.Items.Add('Delphi 10.3 Rio')
+    else if oFRCE.Installations[iFor].VersionNumberStr = 'd27' then
+      edtDelphiVersion.Items.Add('Delphi 10.4 Sydney');
 
     // -- Evento disparado antes de iniciar a execução do processo.
     oFRCE.Installations[iFor].DCC32.OnBeforeExecute := BeforeExecute;
