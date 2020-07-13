18a1771c392f3d7704cf5f07fbe3bc62f245a954
Guilherme Verardi <46457332+guibverardi@users.noreply.github.com>
2020-07-13 10:49:53 -0300

    Correção DisplayMask

--- Source/RLUtils.pas
+++ Source/RLUtils.pas
@@ -1096,7 +1096,7 @@ function SmartGetFieldDisplayText(Field: TField; const Mask: string = ''): Strin
 begin
   if (Field is TBlobField) and not Assigned(Field.OnGetText) then
     Result := Field.AsString
-  else if (Field is TFloatField) and (Mask <> '') then
+  else if (Field is TNumericField) and (Mask <> '') then
     Result := FormatFloat(Mask, Field.AsFloat)
   else
     Result := Field.DisplayText;
